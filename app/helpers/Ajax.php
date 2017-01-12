611636afc1de5ef1676bed6d54cd524e1b41267e
KolBbl4 <ko1ba@mail.ru>
2017-01-12 20:19:14 +0500

    Создан хэлпер(помощник) для работы с аякс запросом

---
+++ app/helpers/Ajax.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\helpers;
+/**
+ * Класс для создание ajax запросов
+ */
+class Ajax {
+ use \app\Singleton;
+ 
+ /**
+  * Функции приёма и обработки запросов
+  */
+ public function  getAjax () {
+   
+   /**
+    * Реализовать
+    */
+ }
+  
+}
